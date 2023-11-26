0012db7b11c8f1e9fb91dcd958b9ffc533668beb
yercin <yer-cin@hotmail.com>
2023-11-25 20:01:56 -0500

    first commit

---
+++ index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+    <link rel="stylesheet" href="css/index.css">
+</head>
+<body>
+
+    <nav class="navbar navbar-expand-lg">
+        <div class="navbar-brand">
+            <img src="img/user.png" alt="Foto del empleado" width="40" height="40">
+            <a href="#" class="d-none d-md-inline">Inicio</a>
+        </div>
+        <div class="navbar-nav">
+            <a href="#" id="cerrar-sesion"><i class="fas fa-sign-out-alt"></i> Cerrar Sesión</a>
+        </div>
+    </nav>
+
+    <div class="options-container">
+        <div class="option-box">
+            <a href="ruta-para-ver-incapacidades.html" class="option-link">
+                <div class="option-image">
+                    <img src="img/shinc.png" alt="Ver Incapacidades">
+                </div>
+                <div class="option-text">
+                    <p class="option-title">Ver Incapacidades</p>
+                </div>
+            </a>
+        </div>
+        <div class="option-box">
+            <a href="ruta-para-reportar-incapacidad.html" class="option-link">
+                <div class="option-image">
+                    <img src="img/newinc.png" alt="Reportar Incapacidad">
+                </div>
+                <div class="option-text">
+                    <p class="option-title">Reportar Incapacidad</p>
+                </div>
+            </a>
+        </div>
+        <!-- Agrega más opciones según sea necesario -->
+    </div>
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+</body>
+</html>
